92d667bfce791b3a1c490bfbb67fab249a05cfee
Junxian He <junxianh2@gmail.com>
2018-11-17 22:17:36 -0500

    first commit copy struct-learning-with-flow repo

---
+++ README.md
@@ -0,0 +1,112 @@
+# struct-learning-with-flow
+
+This is PyTorch implementation of the [paper](https://arxiv.org/abs/1808.09111):
+```
+Unsupervised Learning of Syntactic Structure with Invertible Neural Projections
+Junxian He, Graham Neubig, Taylor Berg-Kirkpatrick
+EMNLP 2018
+```
+
+The code performs unsupervised structure learning with flow, specifically on Markov structure and dependency structure.
+
+Please concact junxianh@cs.cmu.edu if you have any questions.
+
+## Requirements
+
+- Python 3.6
+- PyTorch >=0.4
+- [scikit-learn](http://scikit-learn.org/stable/) (for tagging task only)
+- [NLTK](https://www.nltk.org/) (for parsing task only)
+
+## Data
+Throughout two tasks we use simplified CoNLL format as data input that contains four columns:
+```
+ID Token Tag Head
+```
+At training time only `Token` is used, `Head` represents the dependency head index (for evaluation of parsing task). `Tag` is used for evaluation of tagging task.
+
+As observations in our generative model, pre-trained word vectors are required. The input word2vec map should be a pickled representation of Python dict object.
+
+We provide the pre-trained word vector file we used in the paper and a small subset of Penn Treebank data ([HERE](https://drive.google.com/file/d/18f61nN7l-Dvzqys7BypCsaCcj8gay7ip/view?usp=sharing)) for testing the tagging code. This dataset contains 10% samples of Penn Treebank and is public in [NLTK corpus](http://www.nltk.org/howto/corpus.html). Full Penn Treebank dataset requires a LDC license.
+
+## Markov Structure for Tagging
+
+### Training
+
+Train a Gaussian HMM baseline: 
+
+```shell
+python markov_flow_train.py --model gaussian --train_file /path/to/train --word_vec /path/to/word_vec_file
+```
+
+By default we evaluate on the training data (this is not cheating in unsupervised learning case),  different test dataset can be specified by `--test_file` option. Training uses GPU when there is GPU available,  and CPU otherwise, but running on CPU can be extremely slow. Full configuration options can be found in `markov_flow_train.py`. After training the trained model will be saved in `dump_models/markov/`.
+
+Unsupervised learning is usually very sensitive to initializations, for this task we run multiple random restarts and pick the one with the highest training data likelihood as described in paper. It is generally sufficient to run 10 random restarts. When running with multiple random restarts, it is necessary to specify the `--jobid` or `--taskid` options to avoid model overwriting.
+
+After training the Gaussian HMM, train a projection model with Markov prior:
+
+```shell
+python markov_flow_train.py \
+        --model nice \
+        --train_file /path/to/train \
+        --word_vec /path/to/word_vec_file \
+        --load_gaussian /path/to/gaussian_model 
+```
+
+Initializing the prior with pre-trained Gaussian baseline would make the training much more stable. By default 4 coupling layers are used in NICE projection. 
+
+### Results
+
+On the provided subset of Penn Treebank that contains 3914 sentences, the Gaussian HMM is able to achieve ~76.5% M1 accuracy and ~0.692 VM score, and the projection model (4 layers) achieves ~79.2% M1 accuracy and ~0.718 VM score.
+
+### Prediction
+
+After training, prediction can be performed with :
+
+```shell
+python markov_flow_train.py --model nice --train_file /path/to/tag_file --tag_from /path/to/pretrained_model
+```
+
+Here `--train_file` represents the file to be tagged, the output file is located in the current directory. 
+
+
+
+
+## DMV Structure for Parsing
+### Training
+
+First train a vanilla DMV model with viterbi EM (this only runs on CPU):
+
+```shell
+python dmv_viterbi_train.py --train_file /path/to/train_data --test_file /path/to/test_data
+```
+
+Trained model is saved in `dump_models/dmv/viterbi_dmv.pickle`. Implementation of this basic DMV training is partially based on [this repo](https://github.com/davidswelt/dmvccm).
+
+
+
+Then use the pre-trained DMV to initialize the syntax model in flow/Gaussian model:
+
+```shell
+python dmv_flow_train.py \
+        --model nice \
+        --train_file /path/to/train_data \
+        --test_file /path/to/test_data \
+        --word_vec /path/to/word_vec_file \
+        --load_viterbi_dmv dump_models/dmv/viterbi_dmv.pickle
+```
+
+The script trains a Gaussian baseline when `--model` is specified as `gaussian`. Training uses GPU when there is GPU available,  and CPU otherwise. Trained model is saved in `dump_models/dmv/`.
+
+
+
+## Reference
+```
+@inproceedings{he2018unsupervised,
+    title = {Unsupervised Learning of Syntactic Structure with Invertible Neural Projections},
+    author = {Junxian He and Graham Neubig and Taylor Berg-Kirkpatrick},
+    booktitle = {Proceedings of EMNLP},
+    year = {2018}
+}
+```
+
---
+++ modules/utils.py
@@ -0,0 +1,236 @@
+import torch
+from collections import defaultdict
+import math
+import numpy as np
+from math import log
+
+class ConllSent(object):
+    """docstring for ConllSent"""
+    def __init__(self, key_list=["word", "tag", "head"]):
+        super(ConllSent, self).__init__()
+        self.sent_dict = {}
+        self.keys = key_list
+        for key in key_list:
+            self.sent_dict[key] = []
+
+    def __getitem__(self, key):
+        return self.sent_dict[key]
+
+    def __setitem__(self, key, item):
+        self.sent_dict[key] = item
+
+    def __len__(self):
+        return len(self.sent_dict["word"])
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+def cast_to_int(s):
+    try:
+        return int(s)
+    except ValueError:
+        return s
+
+def word2id(sentences):
+    """map words to word ids
+
+    Args:
+        sentences: a nested list of sentences
+
+    """
+    ids = defaultdict(lambda: len(ids))
+    id_sents = [[ids[word] for word in sent] for sent in sentences]
+    return id_sents, ids
+
+# Compute log sum exp in a numerically stable way for the forward algorithm
+def log_sum_exp(value, dim=None, keepdim=False):
+    """Numerically stable implementation of the operation
+
+    value.exp().sum(dim, keepdim).log()
+    """
+    if dim is not None:
+        m, _ = torch.max(value, dim=dim, keepdim=True)
+        value0 = value - m
+        if keepdim is False:
+            m = m.squeeze(dim)
+        return m + torch.log(torch.sum(torch.exp(value0), dim=dim, keepdim=keepdim))
+    else:
+        m = torch.max(value)
+        sum_exp = torch.sum(torch.exp(value - m))
+        return m + torch.log(sum_exp)
+
+def sents_to_vec(vec_dict, sentences):
+    """read data, produce training data and labels.
+
+    Args:
+        vec_dict: a dict mapping words to vectors.
+        sentences: A list of ConllSent objects
+
+    Returns:
+        embeddings: a list of tensors
+        tags: a nested list of gold tags
+    """
+    embeddings = []
+    for sent in sentences:
+        sample = [vec_dict[word] for word in sent["word"]]
+        embeddings.append(sample)
+
+    return embeddings
+
+def sents_to_tagid(sentences):
+    """transform tagged sents to tagids,
+    also return the look up table
+    """
+    ids = defaultdict(lambda: len(ids))
+    id_sents = [[ids[tag] for tag in sent["tag"]] for sent in sentences]
+    return id_sents, ids
+
+def read_conll(fname, max_len=1e3, rm_null=True, prc_num=True):
+    sentences = []
+    sent = ConllSent()
+
+    null_total = []
+    null_sent = []
+    loc = 0
+    with open(fname) as fin:
+        for line in fin:
+            if line != '\n':
+                line = line.strip().split('\t')
+                sent["head"].append((int(line[0]), 
+                    cast_to_int(line[3])))
+                if rm_null and line[2] == '-NONE-':
+                    null_sent.append(loc)
+                else:
+                    sent["tag"].append(line[2])
+                    if prc_num and is_number(line[1]):
+                        sent["word"].append('0')
+                    else:
+                        sent["word"].append(line[1])
+
+                loc += 1
+            else:
+                loc = 0
+                if len(sent) > 0 and len(sent) <= max_len:
+                    sentences.append(sent)
+                    null_total.append(null_sent)
+
+                null_sent = []
+                sent = ConllSent()
+
+    return sentences, null_total
+
+def write_conll(fname, sentences, pred_tags, null_total):
+    with open(fname, 'w') as fout:
+        for (pred, null_sent, sent) in zip(pred_tags, null_total, sentences):
+            word_list = sent["word"]
+            head_list = sent["head"]
+            length = len(sent) + len(null_sent)
+            assert (length == len(head_list))
+            pred_tag_list = [str(k.item()) for k in pred]
+            for null in null_sent:
+                pred_tag_list.insert(null, '-NONE-')
+                word_list.insert(null, '-NONE-')
+
+            for i in range(length):
+                fout.write("{}\t{}\t{}\t{}\n".format(
+                    i+1, word_list[i], pred_tag_list[i], 
+                    head_list[i][1]))
+            fout.write('\n')
+
+def input_transpose(sents, pad):
+    max_len = max(len(s) for s in sents)
+    batch_size = len(sents)
+
+    sents_t = []
+    masks = []
+    for i in range(max_len):
+        sents_t.append([sent[i] if len(sent) > i else pad for sent in sents])
+        masks.append([1 if len(sent) > i else 0 for sent in sents])
+
+    return sents_t, masks
+
+def to_input_tensor(sents, pad, device):
+    """
+    return a tensor of shape (src_sent_len, batch_size)
+    """
+
+    sents, masks = input_transpose(sents, pad)
+
+
+    sents_t = torch.tensor(sents, dtype=torch.float32, requires_grad=False, device=device)
+    masks_t = torch.tensor(masks, dtype=torch.float32, requires_grad=False, device=device)
+
+    return sents_t, masks_t
+
+def data_iter(data, batch_size, is_test=False, shuffle=True):
+    index_arr = np.arange(len(data))
+    # in_place operation
+
+    if shuffle:
+        np.random.shuffle(index_arr)
+
+    batch_num = int(np.ceil(len(data) / float(batch_size)))
+    for i in range(batch_num):
+        batch_ids = index_arr[i * batch_size: (i + 1) * batch_size]
+        batch_data = [data[index] for index in batch_ids]
+
+        if is_test:
+            # batch_data.sort(key=lambda e: -len(e[0]))
+            test_data = [data_tuple[0] for data_tuple in batch_data]
+            tags = [data_tuple[1] for data_tuple in batch_data]
+
+
+            yield test_data, tags
+
+        else:
+            # batch_data.sort(key=lambda e: -len(e))
+            yield batch_data
+
+def generate_seed(data, size, shuffle=True):
+    index_arr = np.arange(len(data))
+    # in_place operation
+
+    if shuffle:
+        np.random.shuffle(index_arr)
+
+    seed = [data[index] for index in index_arr[:size]]
+
+    return seed
+
+def get_tag_set(tag_list):
+    tag_set = set()
+    tag_set.update([x for s in tag_list for x in s])
+    return tag_set
+
+def stable_math_log(val, default_val=-1e20):
+    if val == 0:
+        return default_val
+
+    return math.log(val)
+
+def unravel_index(input, size):
+    """Unravel the index of tensor given size
+    Args:
+        input: LongTensor
+        size: a tuple of integers
+
+    Outputs: output,
+        - **output**: the unraveled new tensor
+
+    Examples::
+        <<< value = torch.LongTensor(4,5,7,9)
+        <<< max_val, flat_index = torch.max(value.view(4, 5, -1), dim=-1)
+        <<< index = unravel_index(flat_index, (7, 9))
+        <<< # output is a tensor with size (4, 5, 2)
+
+    """
+    idx = []
+    for adim in size[::-1]:
+        idx.append((input % adim).unsqueeze(dim=-1))
+        input = input / adim
+    idx = idx[::-1]
+    return torch.cat(idx, -1)
