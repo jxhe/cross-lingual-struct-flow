3faf2dc24d3889603585abec94d3b3c8f80d5b3b
Junxian He <junxianh2@gmail.com>
2018-11-20 01:05:03 -0500

    modify es config

--- config/config_es.py
+++ config/config_es.py
@@ -1,13 +1,13 @@
 
 params={
-    "couple_layers": 4,
+    "couple_layers": 8,
     "cell_layers": 1,
     "valid_nepoch": 1,
     "epochs": 50,
     "batch_size": 16,
     "emb_dir": "fastText_data",
-    "train_file": "ud-treebanks-v2.2/UD_Spanish-GSD/es_gsd-ud-train.conllu",
-    "val_file":"ud-treebanks-v2.2/UD_Spanish-GSD/es_gsd-ud-dev.conllu",
+    "train_file": "ud-treebanks-v2.2/UD_Spanish-GSD/es_gsd-ud-test.conllu",
+    "val_file":"ud-treebanks-v2.2/UD_Spanish-GSD/es_gsd-ud-test.conllu",
     "test_file":"ud-treebanks-v2.2/UD_Spanish-GSD/es_gsd-ud-test.conllu",
     "vec_file": "fastText_data/wiki.es.gsd.vec",
     "align_file": "multilingual_trans/alignment_matrices/es.txt"
