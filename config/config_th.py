e45f7f761dcaf38cd880c48a843cea5043fbfd49
Junxian He <junxianh2@gmail.com>
2018-11-20 01:15:10 -0500

    add th configuration

---
+++ config/config_th.py
@@ -0,0 +1,14 @@
+
+params={
+    "couple_layers": 4,
+    "cell_layers": 1,
+    "valid_nepoch": 1,
+    "epochs": 50,
+    "batch_size": 16,
+    "emb_dir": "fastText_data",
+    "train_file": "ud-treebanks-v2.2/UD_Thai-PUD/th_pud-ud-test.conllu",
+    "val_file":"ud-treebanks-v2.2/UD_Thai-PUD/th_pud-ud-test.conllu",
+    "test_file":"ud-treebanks-v2.2/UD_Thai-PUD/th_pud-ud-test.conllu",
+    "vec_file": "fastText_data/wiki.th.pud.vec",
+    "align_file": "multilingual_trans/alignment_matrices/th.txt"
+}
