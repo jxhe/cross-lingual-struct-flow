6f8f890c1066ee3aabe715968857aa9da9c0cdc5
Junxian He <junxianh2@gmail.com>
2018-11-20 00:00:27 -0500

    add spanish configure file

---
+++ config/config_es.py
@@ -0,0 +1,15 @@
+
+params={
+    "couple_layers": 4,
+    "cell_layers": 1,
+    "valid_nepoch": 1,
+    "epochs": 50,
+    "batch_size": 16,
+    "lr": 0.001,
+    "emb_dir": "fastText_data",
+    "train_file": "ud-treebanks-v2.2/UD_Spanish-GSD/es_gsd-ud-train.conllu",
+    "val_file":"ud-treebanks-v2.2/UD_Spanish-GSD/es_gsd-ud-dev.conllu",
+    "test_file":"ud-treebanks-v2.2/UD_Spanish-GSD/es_gsd-ud-test.conllu",
+    "vec_file": "fastText_data/wiki.es.gsd.vec",
+    "align_file": "multilingual_trans/alignment_matrices/es.txt"
+}
