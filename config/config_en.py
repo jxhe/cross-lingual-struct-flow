94d6d24a3f3eb7739110098bf7141576e695903d
jxhe <junxianh2@gmail.com>
2018-11-19 23:18:40 -0500

    minor

--- config/config_en.py
+++ config/config_en.py
@@ -10,6 +10,6 @@ params={
     "train_file": "ud-treebanks-v2.2/UD_English-EWT/en_ewt-ud-train.conllu",
     "val_file":"ud-treebanks-v2.2/UD_English-EWT/en_ewt-ud-dev.conllu",
     "test_file":"ud-treebanks-v2.2/UD_English-EWT/en_ewt-ud-test.conllu",
-    "vec_file": "fastText_data/wiki.en.vec",
+    "vec_file": "fastText_data/wiki.en.ewt.vec",
     "align_file": "multilingual_trans/alignment_matrices/en.txt"
 }
