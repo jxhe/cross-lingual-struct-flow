8154773ee02921e6fe844a9c384f07b5670c8eed
Junxian He <junxianh2@gmail.com>
2018-11-19 22:03:53 -0500

    load fasttext model

--- scripts/compress_vec_dict.py
+++ scripts/compress_vec_dict.py
@@ -11,6 +11,7 @@ import argparse
 import importlib
 from io import open
 
+import fasttext
 from modules import *
 from multilingual_trans.fasttext import FastVector
 
@@ -23,8 +24,8 @@ config_file = "config.config_{}".format(args.lang)
 params = importlib.import_module(config_file).params
 args = argparse.Namespace(**vars(args), **params)
 
-word_vec_dict = FastVector(vector_file="fastText_data/wiki.{}.vec".format(args.lang))
-ndim = word_vec_dict.n_dim
+word_vec_dict = model.load_model("fastText_data/wiki.{}.bin".format(args.lang))
+ndim = len(word_vec_dict["is"])
 
 train_text, train_tags = read_conll(args.train_file)
 val_text, val_tags = read_conll(args.val_file)
@@ -38,21 +39,25 @@ _ = [[vocab.update([word]) for word in sent] for sent in test_text]
 print("vocab length {}".format(len(vocab)))
 
 # compute mean
-cnt = 0
-oov = []
-for word in vocab:
-    if word in word_vec_dict:
-        if cnt == 0:
-            sum_ = word_vec_dict[word]
-        else:
-            sum_ += word_vec_dict[word]
-        cnt += 1
-    else:
-        oov.append(word)
-        print("oov: {}".format(word))
+# cnt = 0
+# oov = set()
+# for word in vocab:
+#     if word in word_vec_dict:
+#         if cnt == 0:
+#             sum_ = word_vec_dict[word]
+#         else:
+#             sum_ += word_vec_dict[word]
+#         cnt += 1
+#     else:
+#         oov.update([word])
 
-print("out of vocab # {}".format(len(oov)))
-mean = sum_ / cnt
+# print("out of vocab # {}".format(len(oov)))
+
+# cnt = 0
+# for sent in train_text:
+#     for word in sent:
+#         if word in 
+# mean = sum_ / cnt
 
 suffix = args.train_file.split('/')[-1].split('-')[0].split('_')[1]
 out_file = "fastText_data/wiki.{}.{}.vec".format(args.lang, suffix)
@@ -60,10 +65,6 @@ with open(out_file, "w", encoding="utf-8") as fout:
     fout.write("{} {}\n".format(len(vocab), ndim))
     for word in vocab:
         fout.write("{} ".format(word))
-        if word in word_vec_dict:
-            for val in word_vec_dict[word]:
-                fout.write("{} ".format(val))
-        else:
-            for val in mean:
-                fout.write("{} ".format(val))
+        for val in word_vec_dict[word]:
+            fout.write("{} ".format(val))
         fout.write('\n')
